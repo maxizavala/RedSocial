00338d957dab77eaab008e0d14162fc9e1fa67d0
LeanApa <lea.apagro@gmail.com>
2022-04-21 21:37:38 -0300

    Se crea correctamente la clase reaccion

--- RedSocial/Reaccion.cs
+++ RedSocial/Reaccion.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace RedSocial
 {
-    internal class Class1
+    public class Reaccion
     {
+        public int id { get; set; }
+        public int tipo { get; set; }
+        public Post post { get; set; }
+        public Usuario usuario{ get; set; }
+
+        public Reaccion(int id, int tipo, Post post, Usuario usuario)
+        {
+            this.id = id;
+            this.tipo = tipo;
+            this.post = post;
+            this.usuario = usuario;
+        }
+
     }
 }
