d53d3e212120df99ec1358be7e0c165ab86bf991
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2022-04-24 18:11:26 -0300

    Se agrega branch y se completa clase usuario con props, listas y constructor

--- RedSocial/Usuario.cs
+++ RedSocial/Usuario.cs
@@ -8,17 +8,34 @@ namespace RedSocial
 {
     public class Usuario
     {
+
+        public int id { get; set; }
+        public string dni { get; set; }
         public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string mail { get; set; }
         public string pass { get; set; }
-        public int id { get; }
+        public int intentosFallidos { get; set; }
+        public bool bloqueado { get; set; }
+
+        public List<Usuario> amigos = new List<Usuario>();
+        public List<Post> misPost = new List<Post>();
+        public List<Comentario> misComentarios = new List<Comentario>();
+        public List<Reaccion> misReacciones = new List<Reaccion>();
 
         public Usuario() { }
 
-        public Usuario(int id, string Nombre, string Pass) 
+        public Usuario(int id, string dni, string nombre, string apellido, string mail, string pass, 
+            int intentosFallidos, bool bloqueado) 
         {
             this.id = id;
-            nombre = Nombre;
-            pass = Pass;
+            this.dni = dni;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.mail = mail;
+            this.pass = pass;
+            this.intentosFallidos = intentosFallidos;
+            this.bloqueado = bloqueado;
         }
     }
 }
