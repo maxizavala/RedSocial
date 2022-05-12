a9c3c188ac9716593b3f2d5a025f845805f1a1d2
Florencia Romano <49414741+Erianhel@users.noreply.github.com>
2022-05-12 12:33:24 -0300

    Fix commit anterior

--- RedSocial/RedSocial.cs
+++ RedSocial/RedSocial.cs
@@ -119,7 +119,7 @@ namespace RedSocial
             {
                 if (usuario.id == id)
                 {
-                    quitarAmigo(usuario);
+                    quitarAmigo(usuario); 
                 }
             }
         }
